a82a0b571d646ed11104b6e5792ef5708aad0fb0
pingmado <75196272+ping2109@users.noreply.github.com>
2021-09-07 11:09:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ sudo apt -y install git automake lzop bison gperf build-essential zip \
  curl zlib1g-dev g++-multilib libxml2-utils bzip2 libbz2-dev libbz2-1.0 \
  libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make \
  optipng bc libstdc++6 libncurses5 wget python3 python3-pip python gcc clang  \
- libssl-dev rsync flex git-lfs libz3-dev libz3-4 axel tar gcc llvm lld g++-multilib clang default-jre libxml2
+ libssl-dev rsync flex git-lfs libz3-dev libz3-4 axel tar gcc llvm lld g++-multilib clang default-jre libxml2 ccache
 
 }
 

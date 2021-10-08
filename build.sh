22c0aa03db7431b2a8bc8df666ee20d3b44cc52b
pingmado <75196272+ping2109@users.noreply.github.com>
2021-10-08 09:26:37 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,11 +21,6 @@ cdir() {
 }
 
 installDependencies(){	
-sudo rm -rf /usr/share/dotnet /etc/mysql /etc/php /etc/apt/sources.list.d
-sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
-sudo -E apt-get update
-sudo -E apt-get -y autoremove --purge
-sudo -E apt-get clean 
 sudo apt -y update 
 sudo apt -y install git automake lzop bison gperf build-essential zip \
  curl zlib1g-dev g++-multilib libxml2-utils bzip2 libbz2-dev libbz2-1.0 \
@@ -40,7 +35,7 @@ installDependencies
 
 ## clone Kernel
 echo "Cloning Kernel"
-git clone https://github.com/samsungexynos7870/android_kernel_samsung_exynos7870 -b simple_q_permissive kernel
+git clone https://github.com/ping2109/Ares_7870 -b 120hz kernel
 
 ##------------------------------------------------------##
 ##----------Basic Informations, COMPULSORY--------------##

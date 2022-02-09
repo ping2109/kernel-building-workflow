58d2778c4d498c0093d6519c2fd5842917091581
pingmado <75196272+ping2109@users.noreply.github.com>
2022-02-09 18:48:24 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -376,11 +376,7 @@ build_kernel() {
 	elif [ $COMPILER = "gcc" ]
 	then
 		make -j"$PROCS" O=out \
-				CROSS_COMPILE_ARM32=arm-eabi- \
-				CROSS_COMPILE=aarch64-elf- \
-				AR=aarch64-elf-ar \
-				OBJDUMP=aarch64-elf-objdump \
-				STRIP=aarch64-elf-strip
+				CROSS_COMPILE=arm-eabi-
 
 	elif [ $COMPILER = "clangxgcc" ]
 	then
